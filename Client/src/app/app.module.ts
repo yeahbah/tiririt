e8456ddbb6f09a6a83331e251eb2aa8711d41d19
Arnold Diaz <yeahbah@outlook.com>
2020-05-22 13:45:09 -0600

    Added material UI, bootstrap, navigation and home component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
